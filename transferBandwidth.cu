6d181c56e0000b780d55735ca5644b7ec5b36206
jack <jackmay.zhang@mail.utoronto.ca>
2020-12-08 17:16:53 -0500

    adding kernal to unmanaged transfer

--- transferBandwidth.cu
+++ transferBandwidth.cu
@@ -4,24 +4,25 @@
 
 #define PG (4*1024)
 
+__global__ void add_kern(float *x)
+{
+  for (int i = 0; i < 9; i++) { 
+    x[i] += (int)(1024*pow(2.0,(i+2)));
+  }
+
+}
+
 int main(void)
 {
   int N = 2044*1024;
   float *x, *d_x;
 
   x = (float*)malloc(N*sizeof(float));
- 
   cudaMalloc(&d_x, N*sizeof(float));
 
-  for (int i = 0; i < N; i++) {
-    x[i] = 0;
-  }
-
-  int current = 0;
   for (int i = 0; i < 9; i++) { 
     cudaMemcpy((d_x+current), (x+current), (int)(1024*pow(2.0,(i+2))), cudaMemcpyHostToDevice);
-    x[i] += (int)(1024*pow(2.0,(i+2)));
-    current += (int)(1024*pow(2.0,(i+2)));
+    add_kern<<<10,10>>>(x);
   }
   
   // Free memory
