0caf62ba625e1f42365bac2c58642760597d5541
jack <jackmay.zhang@mail.utoronto.ca>
2020-12-08 18:08:25 -0500

    please

--- transferBandwidth.cu
+++ transferBandwidth.cu
@@ -1,4 +1,5 @@
 #define PG (4*1024)
+#include <stdio.h>
 
 int main(void)
 {
@@ -13,11 +14,22 @@ int main(void)
     x[i] = 0;
   }
 
+
+	cudaEvent_t start, end;
+	float time;
   int current = 0;
+  cudaEventCreate(&start);
+  cudaEventCreate(&end);
+  cudaEventRecord(start);
   for (int i = 0; i < 9; i++) { 
     cudaMemcpy((d_x+current), (x+current), (int)(1024*pow(2.0,(i+2))), cudaMemcpyHostToDevice);
     current += (int)(1024*pow(2.0,(i+2)));
   }
+  cudaEventRecord(end);
+  cudaEventSynchronize(end);
+  cudaEventElapsedTime(&time, start, end);
+
+  printf("time: %f\n", time);
   
   // Free memory
   cudaFree(d_x);
