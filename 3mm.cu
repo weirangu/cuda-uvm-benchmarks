5a903a78e8efc7bccc3999613f5b467f80834bde
jack <jackmay.zhang@mail.utoronto.ca>
2020-12-09 21:01:21 -0500

    3mm managed and unman

--- 3mm.cu
+++ 3mm.cu
@@ -15,6 +15,8 @@
 #include <sys/time.h>
 #include <cuda.h>
 
+#include "common/polybenchUtilFuncts.h"
+
 #define GPU_DEVICE 0
 
 //define the error threshold for the results "not matching"
@@ -33,6 +35,8 @@
 
 /* Can switch DATA_TYPE between float and double */
 typedef float DATA_TYPE;
+
+
 
 void init_array(DATA_TYPE* A, DATA_TYPE* B, DATA_TYPE* C, DATA_TYPE* D)
 {
@@ -70,6 +74,37 @@ void init_array(DATA_TYPE* A, DATA_TYPE* B, DATA_TYPE* C, DATA_TYPE* D)
 		}
 	}
 }
+
+
+void compareResults(DATA_TYPE *G, DATA_TYPE *G_outputFromGpu)
+{
+	int i,j,fail;
+	fail = 0;
+
+	for (i=0; i < NI; i++)
+	{
+		for (j=0; j < NL; j++)
+		{
+			if (percentDiff(G[i*NL + j], G_outputFromGpu[i*NL + j]) > PERCENT_DIFF_ERROR_THRESHOLD)
+			{
+				fail++;				
+			}
+		}
+	}
+	
+	// print results
+	printf("Non-Matching CPU-GPU Outputs Beyond Error Threshold of %4.2f Percent: %d\n", PERCENT_DIFF_ERROR_THRESHOLD, fail);
+}
+
+
+void GPU_argv_init()
+{
+	cudaDeviceProp deviceProp;
+	cudaGetDeviceProperties(&deviceProp, GPU_DEVICE);
+	printf("setting device %d with name %s\n",GPU_DEVICE,deviceProp.name);
+	cudaSetDevice( GPU_DEVICE );
+}
+
 	
 __global__ void mm3_kernel1(DATA_TYPE *A, DATA_TYPE *B, DATA_TYPE *E)
 {
@@ -119,32 +154,104 @@ __global__ void mm3_kernel3(DATA_TYPE *E, DATA_TYPE *F, DATA_TYPE *G)
 }
 
 
-void mm3Cuda(DATA_TYPE* A_gpu, DATA_TYPE* B_gpu, DATA_TYPE* C_gpu, DATA_TYPE* D_gpu, DATA_TYPE* E_gpu, DATA_TYPE* F_gpu, 
-		DATA_TYPE* G_gpu)
+void mm3_cpu(DATA_TYPE *A, DATA_TYPE *B, DATA_TYPE *C, DATA_TYPE *D, DATA_TYPE *E, DATA_TYPE *F, DATA_TYPE *G)
 {
-	cudaEvent_t start, end;
-	float time;
+	int i,j,k;
+	
+	/* E := A*B */
+	for (i = 0; i < NI; i++)
+	{
+		for (j = 0; j < NJ; j++)
+		{
+			E[i*NJ + j] = 0;
+			for (k = 0; k < NK; ++k)
+			{
+				E[i*NJ + j] += A[i*NK + k] * B[k*NJ + j];
+			}
+		}
+	}
+		
+	/* F := C*D */
+	for (i = 0; i < NJ; i++)
+	{
+		for (j = 0; j < NL; j++)
+		{
+			F[i*NL + j] = 0;
+			for (k = 0; k < NM; ++k)
+			{
+				F[i*NL + j] += C[i*NM + k] * D[k*NL + j];
+			}
+		}
+	}
 
+  	/* G := E*F */
+	for (i = 0; i < NI; i++)
+	{
+		for (j = 0; j < NL; j++)
+		{
+			G[i*NL + j] = 0;
+			for (k = 0; k < NJ; ++k)
+			{
+				G[i*NL + j] += E[i*NJ + k] * F[k*NL + j];
+			}
+		}
+	}
+}
+
+
+void mm3Cuda(DATA_TYPE* A, DATA_TYPE* B, DATA_TYPE* C, DATA_TYPE* D, DATA_TYPE* E, DATA_TYPE* F, 
+		DATA_TYPE* G, DATA_TYPE* G_outputFromGpu)
+{
+	double t_start, t_end;
+
+	DATA_TYPE *A_gpu;
+	DATA_TYPE *B_gpu;
+	DATA_TYPE *C_gpu;
+	DATA_TYPE *D_gpu;
+	DATA_TYPE *E_gpu;
+	DATA_TYPE *F_gpu;
+	DATA_TYPE *G_gpu;
+
+	cudaMalloc((void **)&A_gpu, sizeof(DATA_TYPE) * NI * NK);
+	cudaMalloc((void **)&B_gpu, sizeof(DATA_TYPE) * NK * NJ);
+	cudaMalloc((void **)&C_gpu, sizeof(DATA_TYPE) * NJ * NM);
+	cudaMalloc((void **)&D_gpu, sizeof(DATA_TYPE) * NM * NL);
+	cudaMalloc((void **)&E_gpu, sizeof(DATA_TYPE) * NI * NJ);
+	cudaMalloc((void **)&F_gpu, sizeof(DATA_TYPE) * NJ * NL);
+	cudaMalloc((void **)&G_gpu, sizeof(DATA_TYPE) * NI * NL);
+
+	cudaMemcpy(A_gpu, A, sizeof(DATA_TYPE) * NI * NK, cudaMemcpyHostToDevice);
+	cudaMemcpy(B_gpu, B, sizeof(DATA_TYPE) * NK * NJ, cudaMemcpyHostToDevice);
+	cudaMemcpy(C_gpu, C, sizeof(DATA_TYPE) * NJ * NM, cudaMemcpyHostToDevice);
+	cudaMemcpy(D_gpu, D, sizeof(DATA_TYPE) * NM * NL, cudaMemcpyHostToDevice);
+	cudaMemcpy(E_gpu, E, sizeof(DATA_TYPE) * NI * NJ, cudaMemcpyHostToDevice);
+	cudaMemcpy(F_gpu, F, sizeof(DATA_TYPE) * NJ * NL, cudaMemcpyHostToDevice);
+	cudaMemcpy(G_gpu, G, sizeof(DATA_TYPE) * NI * NL, cudaMemcpyHostToDevice);	
 	
 	dim3 block(DIM_THREAD_BLOCK_X, DIM_THREAD_BLOCK_Y);
 	dim3 grid1((size_t)(ceil( ((float)NJ) / ((float)DIM_THREAD_BLOCK_X) )),(size_t)(ceil((float)NI/ ((float)DIM_THREAD_BLOCK_Y) )));
 	dim3 grid2((size_t)(ceil( ((float)NL) / ((float)DIM_THREAD_BLOCK_X) )),(size_t)(ceil((float)NJ/ ((float)DIM_THREAD_BLOCK_Y) )));
 	dim3 grid3((size_t)(ceil( ((float)NL) / ((float)DIM_THREAD_BLOCK_X) )),(size_t)(ceil((float)NI/ ((float)DIM_THREAD_BLOCK_Y) )));
 
-	cudaEventCreate(&start);
-	cudaEventCreate(&end);
-	cudaEventRecord(start);
+	t_start = rtclock();
 	mm3_kernel1<<<grid1,block>>>(A_gpu, B_gpu, E_gpu);
-	cudaDeviceSynchronize();
+  cudaDeviceSynchronize();
 	mm3_kernel2<<<grid2,block>>>(C_gpu, D_gpu, F_gpu);
-	cudaDeviceSynchronize();
+  cudaDeviceSynchronize();
 	mm3_kernel3<<<grid3,block>>>(E_gpu, F_gpu, G_gpu);
-	cudaDeviceSynchronize();
-	cudaEventRecord(end);
-	cudaEventSynchronize(end);
-	cudaEventElapsedTime(&time, start, end);
+  cudaDeviceSynchronize();
+	t_end = rtclock();
+	cudaMemcpy(G_outputFromGpu, G_gpu, sizeof(DATA_TYPE) * NI * NL, cudaMemcpyDeviceToHost);
 
-	fprintf(stdout, "GPU Runtime: %0.6lfs\n", time);
+	fprintf(stdout, "GPU Runtime: %0.6lfs\n", t_end - t_start);
+	
+	//cudaFree(A_gpu);
+	//cudaFree(B_gpu);
+	//cudaFree(C_gpu);
+	//cudaFree(D_gpu);
+	//cudaFree(E_gpu);
+	//cudaFree(F_gpu);
+	//cudaFree(G_gpu);
 }
 
 
@@ -159,26 +266,35 @@ int main(int argc, char** argv)
 	DATA_TYPE* E;
 	DATA_TYPE* F;
 	DATA_TYPE* G;
+	DATA_TYPE* G_outputFromGpu;
 
-	(DATA_TYPE*)cudaMallocManaged(&A, NI*NK*sizeof(DATA_TYPE));
-	(DATA_TYPE*)cudaMallocManaged(&B, NK*NJ*sizeof(DATA_TYPE));
-	(DATA_TYPE*)cudaMallocManaged(&C, NJ*NM*sizeof(DATA_TYPE));
-	(DATA_TYPE*)cudaMallocManaged(&D, NM*NL*sizeof(DATA_TYPE));
-	(DATA_TYPE*)cudaMallocManaged(&E, NI*NJ*sizeof(DATA_TYPE));
-	(DATA_TYPE*)cudaMallocManaged(&F, NJ*NL*sizeof(DATA_TYPE));
-	(DATA_TYPE*)cudaMallocManaged(&G, NI*NL*sizeof(DATA_TYPE));
+	A = (DATA_TYPE*)malloc(NI*NK*sizeof(DATA_TYPE));
+	B = (DATA_TYPE*)malloc(NK*NJ*sizeof(DATA_TYPE));
+	C = (DATA_TYPE*)malloc(NJ*NM*sizeof(DATA_TYPE));
+	D = (DATA_TYPE*)malloc(NM*NL*sizeof(DATA_TYPE));
+	E = (DATA_TYPE*)malloc(NI*NJ*sizeof(DATA_TYPE));
+	F = (DATA_TYPE*)malloc(NJ*NL*sizeof(DATA_TYPE));
+	G = (DATA_TYPE*)malloc(NI*NL*sizeof(DATA_TYPE));
+	G_outputFromGpu = (DATA_TYPE*)malloc(NI*NL*sizeof(DATA_TYPE));
 
 	init_array(A, B, C, D);
 
-	mm3Cuda(A, B, C, D, E, F, G);
+	GPU_argv_init();
 
-	cudaFree(A);
-	cudaFree(B);
-	cudaFree(C);
-	cudaFree(D);
-	cudaFree(E);
-	cudaFree(F);
-	cudaFree(G);
+	t_start = rtclock();
+	mm3Cuda(A, B, C, D, E, F, G, G_outputFromGpu);
+	t_end = rtclock();
+
+	fprintf(stdout, "TOTAL Runtime: %0.6lfs\n", t_end - t_start);
+
+	free(A);
+	free(B);
+	free(C);
+	free(D);
+	free(E);
+	free(F);
+	free(G);
+	free(G_outputFromGpu);
 
 	return 0;
 }
