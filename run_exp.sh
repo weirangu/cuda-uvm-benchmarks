8ccc9b838ae12899bc5f7a31b185bc07287fb8bf
jack <jackmay.zhang@mail.utoronto.ca>
2020-12-10 00:29:27 -0500

    minor change

--- run_exp.sh
+++ run_exp.sh
@@ -17,3 +17,16 @@ for i in 100 512 1024 1536 2048 4096; do
     ./2DConvolution 4096 >> 2DConvolution.txt
     ./2DConvolution-unmanaged 4096 >> 2DConvolution-unmanaged.txt
 done
+
+python3 bar plot.py please tests time PLEASE 2DConvolution 3mm
+
+
+for size in 100 512 1024 1536 2048 4096; do
+    ./3mm_man $size >> 3mm.txt
+    ./3mm $size >> 3mm-unmanaged.txt
+    ./2DConvolution $size >> 2DConvolution.txt
+    ./2DConvolution-unmanaged $size >> 2DConvolution-unmanaged.txt
+done
+
+
+python3 line plot.py please tests time PLEASE 2DConvolution 3mm
