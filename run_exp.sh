4799182a9b26ea920bab6ced0a4ca183b4fcb9c9
Weiran Gu <guweiran@teach.cs.toronto.edu>
2020-12-10 00:06:16 -0500

    Removed experiment.py

--- run_exp.sh
+++ run_exp.sh
@@ -12,5 +12,8 @@ make 3mm
 make 3mm_man
 make 2DConvolution
 
-python3 experiment.py
-
+for size in 100 512 1024 1536 2048 4096; do
+    ./3mm_man $size >> 3mm-managed.txt
+    ./3mm $size >> 3mm.txt
+    ./2DConvolution $size >> 2DConvo.txt
+    ./2DConvolution-unmanaged $size >> 2DConvo-managed.txt
