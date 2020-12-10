2c4d37b0dd2ba4f8ea7b3e2f02a1a16a22f97d51
jack <jackmay.zhang@mail.utoronto.ca>
2020-12-10 00:17:08 -0500

    working run_exp

--- run_exp.sh
+++ run_exp.sh
@@ -1,7 +1,4 @@
-rm 3mm.txt
-rm 3mm-unmanaged.txt
-rm 2DConvolution.txt
-rm 2DConvolution-unmanaged.txt
+rm *.txt
 
 rm 3mm
 rm 3mm_man
@@ -12,8 +9,11 @@ make 3mm
 make 3mm_man
 make 2DConvolution
 
-for size in 100 512 1024 1536 2048 4096; do
-    ./3mm_man $size >> 3mm-managed.txt
-    ./3mm $size >> 3mm.txt
-    ./2DConvolution $size >> 2DConvo.txt
-    ./2DConvolution-unmanaged $size >> 2DConvo-managed.txt
+
+#for size in 100 512 1024 1536 2048 4096; do
+for i in 100 512 1024 1536 2048 4096; do
+    ./3mm_man 1024 >> 3mm.txt
+    ./3mm 1024 >> 3mm-unmanaged.txt
+    ./2DConvolution 4096 >> 2DConvolution.txt
+    ./2DConvolution-unmanaged 4096 >> 2DConvolution-unmanaged.txt
+done
