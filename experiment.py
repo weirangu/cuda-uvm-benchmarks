d58373e6c0b23d08d8debd20f373b123f31e812c
jack <jackmay.zhang@mail.utoronto.ca>
2020-12-09 22:48:05 -0500

    please

--- experiment.py
+++ experiment.py
@@ -6,6 +6,9 @@ sizes = [100, 512, 1024, 1536, 2048, 4096]
 for sz in sizes:
   Popen("./3mm_man %i >> 3mm_managed.txt"%(sz), shell=True)
   Popen("./3mm %i >> 3mm.txt"%(sz), shell=True)
+  Popen("./2DConvolution %i >> 2DConvo.txt"%(sz), shell=True)
+  Popen("./2DConvolution-unmanaged %i >> 2DConvo_unman.txt"%(sz), shell=True)
+
   
 
 
--- run_exp.sh
+++ run_exp.sh
@@ -1,10 +1,16 @@
 rm 3mm.txt
 rm 3mm_managed.txt
+rm 2DConvo.txt
+rm 2DConvo_unman.txt
+
 rm 3mm
 rm 3mm_man
+rm 2DConvolution
+rm 2DConvolution-unmanaged
 
 make 3mm
 make 3mm_man
+make 2DConvolution
 
 python3 experiment.py
 
