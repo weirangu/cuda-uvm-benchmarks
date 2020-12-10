f1c34cbee263fe80a91632eb6f4a9f952c3ea70b
jack <jackmay.zhang@mail.utoronto.ca>
2020-12-10 01:18:21 -0500

    what we have on fb wtih the nice graph

--- run_exp.sh
+++ run_exp.sh
@@ -1,4 +1,5 @@
 rm *.txt
+rm *.png
 
 rm 3mm
 rm 3mm_man
@@ -12,16 +13,17 @@ make 2DConvolution
 
 #for size in 100 512 1024 1536 2048 4096; do
 for i in 100 512 1024 1536 2048 4096; do
-    ./3mm_man 1024 >> 3mm.txt
-    ./3mm 1024 >> 3mm-unmanaged.txt
+    ./3mm_man 200 >> 3mm.txt
+    ./3mm 200 >> 3mm-unmanaged.txt
     ./2DConvolution 4096 >> 2DConvolution.txt
     ./2DConvolution-unmanaged 4096 >> 2DConvolution-unmanaged.txt
 done
 
-python3 plot.py bar please tests time bar.png 2DConvolution 3mm
+python3 plot.py bar performance tests time bar.png 2DConvolution 3mm
 
+rm *.txt
 
-for size in 100 512 1024 1536 2048 4096; do
+for size in 50 100 200 250 512 1024; do
     ./3mm_man $size >> 3mm.txt
     ./3mm $size >> 3mm-unmanaged.txt
     ./2DConvolution $size >> 2DConvolution.txt
