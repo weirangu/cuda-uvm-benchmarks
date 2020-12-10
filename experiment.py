19adeb66dba62dad2cd946f7eb3604bfe5032f02
jack <jackmay.zhang@mail.utoronto.ca>
2020-12-09 22:29:37 -0500

    minor change

--- experiment.py
+++ experiment.py
@@ -1,7 +1,7 @@
 from subprocess import Popen, PIPE
 
 
-sizes = [100, 512, 1024, 1536, 2048]
+sizes = [100, 512, 1024, 1536, 2048, 4096]
 
 for sz in sizes:
   Popen("./3mm_man %i >> 3mm_managed.txt"%(sz), shell=True)
